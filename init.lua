77663d444da8cee7473043622ac2bd5c5226b292
LoneWolfHT <lonewolf04361@gmail.com>
2019-11-20 17:12:37 -0800

    Fix bugs and add support for technic

--- init.lua
+++ init.lua
@@ -4,42 +4,37 @@ local overrides = {
 	"bucket_wooden:bucket_water",
 	"bucket_wooden:bucket_river_water",
 	"bucket:bucket_lava",
+	"default:lava_source",
 	"default:water_source",
+	"technic:lava_can",
+	"technic:water_can",
 }
 
 minetest.register_privilege("liquids", {
 	description = "Can place liquids above y 0",
 	give_to_singleplayer = false,
-	give_to_admin = true,
+	give_to_admin = false,
 })
 
-for _, name in ipairs(overrides) do
+for _, name in pairs(overrides) do
 	local def = minetest.registered_items[name]
+	
 
-	if not def then return end -- Item doesn't exist
+	if def then -- Item exists
+		local newdef = {}
+		local old_on_place = def.on_place or function() end
 
-	local old_on_place = def.on_place
-	local newdef = {}
-
-	if not def.on_place then
 		newdef.on_place = function(itemstack, p, pt)
 			if pt.above.y > 0 and not minetest.check_player_privs(p, "liquids") then
 				minetest.chat_send_player(p:get_player_name(), "You do not have the privs required to place liquids above y 0!")
-				return itemstack
-			end
-		end
-	else
-		newdef.on_place = function(itemstack, p, pt)
-			if pt.above.y > 0 and not minetest.check_player_privs(p, "liquids") then
-				minetest.chat_send_player(p:get_player_name(), "You do not have the privs required to place liquids above y 0!")
+				minetest.log("returning...")
 				return itemstack
 			end
 
-			minetest.log("returning...")
 			return old_on_place(itemstack, p, pt)
 		end
-	end
 
-	minetest.override_item(name, newdef)
-	minetest.log("warning", "Overriding "..dump(name))
+		minetest.override_item(name, newdef)
+		minetest.log("warning", "Overriding "..dump(name))
+	end
 end
